b16a9f394780ad293f569d890881483686033161
DanielDomingueti <danieldomingueti@hotmail.com>
2022-01-09 20:18:07 -0300

    RestTemplate Bean | eureka dependency | .yml properties

--- src/main/java/com/domingueti/loja/LojaApplication.java
+++ src/main/java/com/domingueti/loja/LojaApplication.java
@@ -2,9 +2,18 @@ package com.domingueti.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LojaApplication {
+	
+	@Bean
+	@LoadBalanced  //logica para resolver a URL http://fornecedor/info/estado...
+	public RestTemplate getRestTemplate() { //fazer o request ao fornecedor
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
--- src/main/java/com/domingueti/loja/service/CompraService.java
+++ src/main/java/com/domingueti/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package com.domingueti.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,14 @@ import com.domingueti.loja.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+	
 	public void realizaCompra(CompraDTO compra) {
 
-		RestTemplate client = new RestTemplate();
-		
 		ResponseEntity<InfoFornecedorDTO> exchange = 
 		client.exchange(
-				"http://localhost:8081/info/"+compra.getEndereco().getEstado(), 
+				"http://fornecedor:8081/info/"+compra.getEndereco().getEstado(), 
 				HttpMethod.GET, 
 				null, 
 				InfoFornecedorDTO.class
