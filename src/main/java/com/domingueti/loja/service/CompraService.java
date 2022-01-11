959e434443d5ba4105aa1f17ea08fdbfd4de6c3a
DanielDomingueti <danieldomingueti@hotmail.com>
2022-01-11 12:33:25 -0300

    discoveryClient

--- src/main/java/com/domingueti/loja/service/CompraService.java
+++ src/main/java/com/domingueti/loja/service/CompraService.java
@@ -1,6 +1,7 @@
 package com.domingueti.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -8,11 +9,15 @@ import org.springframework.web.client.RestTemplate;
 
 import com.domingueti.loja.dto.CompraDTO;
 import com.domingueti.loja.dto.InfoFornecedorDTO;
-
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 @Service
+@EnableDiscoveryClient
 public class CompraService {
 
+	@Autowired
+	private DiscoveryClient eurekaClient;
+	
 	@Autowired
 	private RestTemplate client;
 	
@@ -25,6 +30,12 @@ public class CompraService {
 				null, 
 				InfoFornecedorDTO.class
 		); 
+		
+		eurekaClient.getInstances("fornecedor").stream()
+		.forEach(fornecedor -> {
+			System.out.println("localhost:"+fornecedor.getPort());
+		});
+		
 		System.out.println(exchange.getBody().getEndereco());
 		
 	}
