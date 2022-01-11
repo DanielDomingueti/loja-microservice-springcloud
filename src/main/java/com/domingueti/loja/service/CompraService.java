b9151b2e0e6a17ba3cd058507980a0e44bc5fb5b
DanielDomingueti <danieldomingueti@hotmail.com>
2022-01-11 13:25:05 -0300

    feign clients

--- src/main/java/com/domingueti/loja/service/CompraService.java
+++ src/main/java/com/domingueti/loja/service/CompraService.java
@@ -2,42 +2,23 @@ package com.domingueti.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.domingueti.loja.client.FornecedorClient;
 import com.domingueti.loja.dto.CompraDTO;
 import com.domingueti.loja.dto.InfoFornecedorDTO;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 @Service
 @EnableDiscoveryClient
 public class CompraService {
 
 	@Autowired
-	private DiscoveryClient eurekaClient;
-	
-	@Autowired
-	private RestTemplate client;
+	private FornecedorClient fornecedorClient;
 	
 	public void realizaCompra(CompraDTO compra) {
 
-		ResponseEntity<InfoFornecedorDTO> exchange = 
-		client.exchange(
-				"http://fornecedor:8081/info/"+compra.getEndereco().getEstado(), 
-				HttpMethod.GET, 
-				null, 
-				InfoFornecedorDTO.class
-		); 
-		
-		eurekaClient.getInstances("fornecedor").stream()
-		.forEach(fornecedor -> {
-			System.out.println("localhost:"+fornecedor.getPort());
-		});
-		
-		System.out.println(exchange.getBody().getEndereco());
-		
+	InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
+	System.out.println(info.getEndereco());
 	}
 
 	
